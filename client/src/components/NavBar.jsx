894fb2efdb972ea6d5dfc25e9eed5b28274422e9
RichardWangg <richardwangg198@gmail.com>
2023-06-20 16:52:06 -0400

    navbar done!

--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -3,6 +3,11 @@ import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import { useState } from "react";
 import { useEffect } from "react";
+import linkedin from '../assets/linkedin.png'
+import github from '../assets/github.png'
+import email from '../assets/email.png'
+import NavbarBrand from 'react-bootstrap/esm/NavbarBrand';
+import my_logo from '../assets/my_logo.png'
 
 
 const NavBar = () => {
@@ -23,21 +28,30 @@ const NavBar = () => {
         return () => window.removeEventListener("scroll", onScroll)
     }, []) //[], the code itself will only run once, it can still check for scrolling though in that one running of it
 
+    const onUpdateActiveLink = (value) => {
+        setActiveLink(value);
+    }
+
+
     return (
-        <Navbar expand="lg" className={scrolled ? "scrolled" : ""}>
+        <Navbar expand="xl" className={scrolled ? "scrolled" : ""}>
             <Container>
-                <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                <Navbar.Brand href="#"> Richard Wang </Navbar.Brand>
+                <Navbar.Toggle aria-controls="basic-navbar-nav" className="ml-auto" />
                 <Navbar.Collapse id="basic-navbar-nav">
-                    <Nav className="me-auto">
-                        <Nav.Link href="#"> Home </Nav.Link>
-                        <Nav.Link href="#projects"> Projects </Nav.Link>
-                        <Nav.Link href="#contact"> Contact </Nav.Link>
-                        <Nav.Link href="#about"> About </Nav.Link>
+                    <Nav className="ms-auto">
+                        <Nav.Link href="#" className={activeLink === 'home' ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateActiveLink('home')}  > Home </Nav.Link>
+                        <Nav.Link href="#projects" className={activeLink === 'projects' ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateActiveLink('projects')}> Projects </Nav.Link>
+                        <Nav.Link href="#contact" className={activeLink === 'contact' ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateActiveLink('contact')}> Contact </Nav.Link>
+                        <Nav.Link href="#about" className={activeLink === 'about' ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateActiveLink('about')}> About </Nav.Link>
+                        <Nav.Link className={activeLink === 'social' ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateActiveLink('social')}>
+                            <div className="social-icon">
+                                <a href='http://www.linkedin.com/in/richard-wang-15230725b'  ><img src={linkedin} alt='LinkedIn' className="image-resize" /></a>
+                                <a href='https://github.com/RichardWangg' ><img src={github} alt='GitHub' className="image-resize" /></a>
+                                <a href='#contact' ><img src={email} alt='' className="image-resize" /></a>
+                            </div>
+                        </Nav.Link>
                     </Nav>
-                    <span>
-
-
-                    </span>
                 </Navbar.Collapse>
             </Container>
         </Navbar>
